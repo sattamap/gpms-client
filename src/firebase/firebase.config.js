d4a3b4c663de140344ab9e9e72afc7ef4f735410
sattamap <sattamap@gmail.com>
2024-01-08 14:45:42 +0600

    feat: integrate Firebase authentication in AuthProvider.jsx

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAOav79LYxhswMDju36XqoLeqcCdWBVe60",
-  authDomain: "gpms-c1d60.firebaseapp.com",
-  projectId: "gpms-c1d60",
-  storageBucket: "gpms-c1d60.appspot.com",
-  messagingSenderId: "575650166062",
-  appId: "1:575650166062:web:e5a205642c817b940cb6bf"
+  apiKey:import.meta.env.VITE_apiKey ,
+  authDomain:import.meta.env.VITE_authDomain ,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId 
 };
 
 // Initialize Firebase
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,9 +1,9 @@
 import { createContext } from "react";
-
-
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
-
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
@@ -15,7 +15,7 @@ const AuthProvider = ({children}) => {
 
     const authInfo = {
       
-        
+
                       };
 
     return (
