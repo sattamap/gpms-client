4d989c1141a0339b825c03f6ca50a88ecd987590
sattamap <sattamap@gmail.com>
2024-01-08 14:33:36 +0600

    feat: initialize Firebase and add Firebase configuration in firebase.config.js

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOav79LYxhswMDju36XqoLeqcCdWBVe60",
+  authDomain: "gpms-c1d60.firebaseapp.com",
+  projectId: "gpms-c1d60",
+  storageBucket: "gpms-c1d60.appspot.com",
+  messagingSenderId: "575650166062",
+  appId: "1:575650166062:web:e5a205642c817b940cb6bf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
