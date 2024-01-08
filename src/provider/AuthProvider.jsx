12d2dd00144d83d678d44e42a47afb1e30b9206e
sattamap <sattamap@gmail.com>
2024-01-08 15:05:47 +0600

    feat: implement user creation and sign-in functions in AuthProvider.jsx

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,22 +1,33 @@
-import { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+    const [loading, setLoading] = useState(true);
 
-    
+    const createUser = (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password)
+    }
   
 
 
  
 
     const authInfo = {
-      
+        loading,
+        createUser,
+        signIn,
 
-                      };
+    };
 
     return (
         <AuthContext.Provider value={authInfo}>
