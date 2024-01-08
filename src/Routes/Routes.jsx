8e21f565934b77c9f678ebb38cc02f2de0d38c90
sattamap <sattamap@gmail.com>
2024-01-08 14:07:28 +0600

    feat: create Main component as the main layout of the project

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main></Main>,
     },
   ]);
---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+    return (
+        <div>
+            <h2>This is my Main.jsx</h2>
+        </div>
+    );
+};
+
+export default Main;
