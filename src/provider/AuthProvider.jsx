8e930e770c69fc4dd3bd30cc87014df87141bee5
sattamap <sattamap@gmail.com>
2024-01-13 20:52:12 +0600

    chore: Add user authentication state listener

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 
@@ -7,6 +7,7 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) =>{
@@ -25,12 +26,28 @@ const AuthProvider = ({children}) => {
           })
           
     }
+
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+          console.log("user in auth state", currentUser);
+          if (currentUser) {
+            setUser(currentUser);
+          }
+          setLoading(false);
+        });
+    
+        return () => {
+          unSubscribe();
+        };
+      }, []);
   
 
 
  
 
     const authInfo = {
+        user,
         loading,
         createUser,
         signIn,
