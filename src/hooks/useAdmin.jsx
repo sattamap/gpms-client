0c321e74586801b587a67e9c2302f678e2d7898f
sattamap <sattamap@gmail.com>
2024-01-13 21:14:27 +0600

    feat: Implement useAdmin custom hook

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+
+
+
+const useAdmin = () => {
+    const { user, loading } = useContext(AuthContext);
+    const axiosPublic = useAxiosPublic();
+    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        enabled: !loading,
+        queryFn: async () => {
+            console.log('checking admin', user)
+            const res = await axiosPublic.get(`/users/admin/${user.email}`);
+            // console.log(res.data);
+            return res.data?.admin;
+            
+        }
+       
+    })
+
+    
+    return [isAdmin, isAdminLoading]
+};
+
+export default useAdmin;
