6706c9670c0435f8a66fc0bcaa2945a7ac910b4b
sattamap <sattamap@gmail.com>
2024-01-27 14:31:33 +0600

    Separate NoRoleWelcome sentences into different divs with different background colors and make them responsive

--- src/pages/Dashboard/NoRole/NoRoleWelcome/NoRoleWelcome.jsx
+++ src/pages/Dashboard/NoRole/NoRoleWelcome/NoRoleWelcome.jsx
@@ -5,13 +5,15 @@ const NoRoleWelcome = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className="bg-slate-200 p-8 md:p-16 shadow-xl rounded-xl">
-      <p className="text-lg md:text-2xl lg:text-3xl font-bold text-center">
+    <div className="p-8 md:p-16 shadow-xl rounded-xl">
+      <div className="bg-blue-200 text-lg md:text-2xl lg:text-3xl font-bold text-center p-4 md:p-8 rounded-md mb-4">
         Welcome!{" "}
         <span className="text-green-500 uppercase">{user?.displayName}</span>{" "}
-        to GPMS of Bangladesh Betar, Bandarban <br /> Your current status is{" "}
-        &apos;none&apos;. Please wait for admin approval to gain access.
-      </p>
+        to GPMS of Bangladesh Betar, Bandarban
+      </div>
+      <div className="bg-red-200 text-lg md:text-2xl lg:text-3xl font-bold text-center p-4 md:p-8 rounded-md">
+        Your current status is &apos;none&apos;. Please wait for admin approval to gain access.
+      </div>
     </div>
   );
 };
