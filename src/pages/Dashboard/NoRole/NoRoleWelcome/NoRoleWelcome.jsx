984e6e24939ceebcbaa8a559bfba5d5930926047
sattamap <sattamap@gmail.com>
2024-01-27 14:27:22 +0600

    Make NoRoleWelcome component responsive with Tailwind CSS

--- src/pages/Dashboard/NoRole/NoRoleWelcome/NoRoleWelcome.jsx
+++ src/pages/Dashboard/NoRole/NoRoleWelcome/NoRoleWelcome.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../../provider/AuthProvider";
 
-
 const NoRoleWelcome = () => {
-  const { user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+
   return (
-    <div className="bg-slate-200 p-16 shadow-xl rounded-xl">
-      <p className="text-2xl font-bold text-center">
-        Welcome! <span className="text-green-500 uppercase">{user?.displayName}</span> to GPMS of Banladesh Betar, Bandarban  <br /> Your current status is &apos;none&apos;. Please wait for admin approval to gain access.
+    <div className="bg-slate-200 p-8 md:p-16 shadow-xl rounded-xl">
+      <p className="text-lg md:text-2xl lg:text-3xl font-bold text-center">
+        Welcome!{" "}
+        <span className="text-green-500 uppercase">{user?.displayName}</span>{" "}
+        to GPMS of Bangladesh Betar, Bandarban <br /> Your current status is{" "}
+        &apos;none&apos;. Please wait for admin approval to gain access.
       </p>
     </div>
   );
